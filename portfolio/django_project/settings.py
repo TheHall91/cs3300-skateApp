6222902d3d40c74f511a1f16c81ab56b18f169e5
TheHall91 <157056297+TheHall91@users.noreply.github.com>
2024-03-30 20:04:22 -0400

    Need to Add Image

--- portfolio/django_project/settings.py
+++ portfolio/django_project/settings.py
@@ -37,7 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'skatepark_app'
 ]
+
+AUTHENTICATION_BACKENDS = [ 'django.contrib.auth.backends.ModelBackend', ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
---
+++ portfolio/skatepark_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class SkateparkAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'skatepark_app'
